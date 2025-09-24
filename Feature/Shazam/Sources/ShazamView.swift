5fe47f132339f4b9f3a4e1d8137e17e800df3ab1
Jung Hwan Park <hwann1005@gmail.com>
2025-09-24 16:00:10 +0900

    Add Shazam functionality

--- Feature/Shazam/Sources/ShazamView.swift
+++ Feature/Shazam/Sources/ShazamView.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 public struct ShazamView: View {
+    @StateObject private var viewModel = ShazamViewModel()
+    
     public init() {}
     
     public var body: some View {
-        VStack {
-            Text("Hello, this is ShazamView")
-            
-            Image("generic_image_a", bundle: .module)
+        NavigationView {
+            VStack(spacing: 8) {
+                Spacer()
+                AsyncImage(url: viewModel.currentItem?.artworkURL) { image in
+                    image.image?.resizable().scaledToFit()
+                }
+                .frame(width: 200, height: 200, alignment: .center)
+                
+                Text(viewModel.currentItem?.title ?? "Press the button below to Shazam")
+                    .font(.title3.bold())
+                
+                Text(viewModel.currentItem?.artist ?? "")
+                    .font(.body)
+                
+                Spacer()
+                
+                Spacer()
+                if viewModel.shazaming {
+                    Button {
+                        viewModel.stopRecognition()
+                    } label: {
+                        Text("Stop Shazam")
+                            .font(.headline.bold())
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 48)
+                            .foregroundStyle(Color.white)
+                            .background(
+                                Capsule(style: .continuous)
+                                    .fill(Color.red)
+                            )
+                    }
+                } else {
+                    Button {
+                        viewModel.startRecognition()
+                    } label: {
+                        Text("Start Shazam")
+                            .font(.headline.bold())
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 48)
+                            .foregroundStyle(Color.white)
+                            .background(
+                                Capsule(style: .continuous)
+                                    .fill(Color.accentColor)
+                            )
+                    }
+                }
+            }
+            .padding(20)
+            .navigationTitle("Shazam")
         }
     }
 }
---
+++ Feature/Shazam/Sources/ShazamViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  ShazamViewModel.swift
+//  Shazam
+//
+//  Created by Jung Hwan Park on 9/23/25.
+//
+
+import AVKit
+import ShazamKit
+import Combine
+
+@MainActor
+class ShazamViewModel: NSObject, ObservableObject {
+    @Published var currentItem: SHMediaItem? = nil
+    @Published var shazaming = false
+    
+    private let session = SHSession()
+    private let audioEngine = AVAudioEngine()
+    
+    override init() {
+        super.init()
+        session.delegate = self
+    }
+    
+    private func prepareAudioRecording() throws {
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+    }
+    
+    private func generateSignature() {
+        let inputNode = audioEngine.inputNode
+        let recordingFormat = inputNode.outputFormat(forBus: .zero)
+        
+        inputNode.installTap(onBus: .zero, bufferSize: 1024, format: recordingFormat) { [weak session] buffer, _ in
+            session?.matchStreamingBuffer(buffer, at: nil)
+        }
+    }
+    
+    private func startAudioRecording() throws {
+        try audioEngine.start()
+        shazaming = true
+    }
+    
+    public func startRecognition() {
+        do {
+            if audioEngine.isRunning {
+                stopRecognition()
+                return
+            }
+            
+            try prepareAudioRecording()
+            generateSignature()
+            try startAudioRecording()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    public func stopRecognition() {
+        shazaming = false
+        audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: .zero)
+    }
+}
+
+extension ShazamViewModel: @preconcurrency SHSessionDelegate {
+    func session(_ session: SHSession, didFind match: SHMatch) {
+        guard let mediaItem = match.mediaItems.first else { return }
+        
+        Task {
+            self.currentItem = mediaItem
+        }
+    }
+}
